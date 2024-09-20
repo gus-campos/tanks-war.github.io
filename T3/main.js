2b367ea52fecb847810d3a90b351e4a0fcef0437
Gustavo Campos <gus.campos042@gmail.com>
2024-09-20 14:29:05 -0300

    html redirect

--- T3/main.js
+++ T3/main.js
@@ -261,7 +261,7 @@ const zoomLevels = [1/6, 1/5, 1/4, 1/3, 1/2, 1, 2, 3, 4, 5, 6];
 reset(1);
 
 // Joystick
-mobileMode = true;
+mobileMode = document.getElementById("main").getAttribute("data-mobile") == "true";
 touchControls = new TouchControls();
 if (!mobileMode) touchControls.deleteUI();
 
